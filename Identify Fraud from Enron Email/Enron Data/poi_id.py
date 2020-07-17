c2781d0f4dcc4f67650942b7fe88700f6aaae855
TrikerDev <trikerdev@gmail.com>
2020-07-17 16:11:51 -0700

    Add existing file

--- Identify Fraud from Enron Email/Enron Data/poi_id.py
+++ Identify Fraud from Enron Email/Enron Data/poi_id.py
@@ -1,81 +1,73 @@
 #!/usr/bin/python
 
+# Importing packages
 import sys
 import pickle
 sys.path.append("Enron Data/")
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
-### Task 1: Select what features you'll use.
 
-
-features_list = [
- 'poi',
- 'deferral_payments',
- 'director_fees',
- 'exercised_stock_options',
- 'from_this_person_to_poi',
- 'loan_advances',
- 'other',
- 'restricted_stock_deferred',
- 'salary',
- 'to_messages',
- 'total_payments',
- 'total_stock_value'
-]
-
-
-
-### Load the dictionary containing the dataset
-
-with open("Enron Data/final_project_dataset.pkl", "r") as data_file:
+# Loading data
+with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
-
-
-
-
-### Task 2: Remove outliers
-
-data_dict.pop('THE TRAVEL AGENCY IN THE PARK', 0)
+    
+    
+# Removing THE TRAVEL AGENCY IN THE PARK outlier
+data_dict.pop('THE TRAVEL AGENCY IN THE PARK', 0) 
+# Removing TOTAL outlier
 data_dict.pop('TOTAL', 0)
+# Removing LOCKHART EUGENE E outlier
 data_dict.pop('LOCKHART EUGENE E', 0)
+# Removing CHAN RONNIE outlier
 data_dict.pop('CHAN RONNIE', 0)
 
 
+# Saving this new dataset without the outliers
 my_dataset = data_dict
 
+features_list = ['poi', 'deferral_payments', 'total_payments', 'exercised_stock_options', 'bonus', 'restricted_stock', 'restricted_stock_deferred', 'expenses', 'director_fees', 'deferred_income']
+
+
+import numpy as np
+np.random.seed(42)
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 
 data = featureFormat(my_dataset, features_list)
 labels, features = targetFeatureSplit(data)
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.4, random_state = 42)
 
-# Importing Nearest K
-from sklearn.neighbors.nearest_centroid import NearestCentroid
-clf = NearestCentroid()
 
-# Training Nearest K
+from sklearn.linear_model import LogisticRegression
+
+
+clf = LogisticRegression(C=0.001, class_weight=None, dual=False, fit_intercept=True,
+          intercept_scaling=1, max_iter=100, multi_class='warn',
+          n_jobs=None, penalty='l2', random_state=None, solver='warn',
+          tol=0.0001, verbose=0, warm_start=False)
 clf = clf.fit(features_train, labels_train)
 pred = clf.predict(features_test)
 
 
-
-# Testing Nearest K
-from sklearn.metrics import accuracy_score
-
 acc = accuracy_score(labels_test, pred)
 
-from sklearn.metrics import precision_score
+print 'Accuracy: ' + str(acc)
+print 'Precision: ', precision_score(labels_test, pred)
+print 'Recall: ', recall_score(labels_test, pred)
 
-prec = precision_score(labels_test, pred)
 
-from sklearn.metrics import recall_score
 
-rec = recall_score(labels_test, pred)
 
-print ('Accuracy Score: ' + str(acc))
-print ('Precision Score: ' + str(prec))
-print ('Recall Score: ' + str(rec))
 
+
+### Task 6: Dump your classifier, dataset, and features_list so anyone can
+### check your results. You do not need to change anything below, but make sure
+### that the version of poi_id.py that you submit can be run on its own and
+### generates the necessary .pkl files for validating your results.
 
 dump_classifier_and_data(clf, my_dataset, features_list)
